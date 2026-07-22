599b8909325ac9ad8ebfe8ababb765b465daaceb
code-tirth <tirthjlimbani@gmail.com>
2026-07-22 09:39:52 +0530

    Create objectclass.py

---
+++ Practice/objectclass.py
@@ -0,0 +1,13 @@
+#Object and CLass
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+s1 = Student("Rahul", 20)
+s1.display()
