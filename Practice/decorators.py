c29a80351436cb93e69d84691b5265160e02e326
code-tirth <tirthjlimbani@gmail.com>
2026-07-22 09:37:56 +0530

    Update and rename Decorators1.py to decorators.py

--- Practice/decorators.py
+++ Practice/decorators.py
@@ -13,3 +13,24 @@ def greet():
 
 # Calling the decorated function
 greet()
+
+def star(func):
+    def wrapper():
+        print("*" * 10)
+        func()
+        print("*" * 10)
+    return wrapper
+
+def hash_line(func):
+    def wrapper():
+        print("#" * 10)
+        func()
+        print("#" * 10)
+    return wrapper
+
+@star
+@hash_line
+def display():
+    print("Python")
+
+display()
